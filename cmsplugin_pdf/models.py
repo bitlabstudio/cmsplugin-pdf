0b13fa58564f098c847b4644e28be56101165df7
Martin Brochhaus <mbrochh@gmail.com>
2013-07-08 02:47:46 +0800

    Fixed bugs with the file saving and thumbnail generation

--- cmsplugin_pdf/models.py
+++ cmsplugin_pdf/models.py
@@ -1,4 +1,5 @@
 """Models for the ``cmsplugin_pdf`` app."""
+import glob
 import os
 
 from django.conf import settings
@@ -9,6 +10,9 @@ from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
 from filer.fields.file import FilerFileField
 from wand.image import Image
+
+
+UPLOAD_TO_DIR = 'pdf_images'
 
 
 class PDFPluginModel(CMSPlugin):
@@ -25,7 +29,7 @@ class PDFPluginModel(CMSPlugin):
 
     image = models.ImageField(
         verbose_name=_('Image'),
-        upload_to=os.path.join(settings.MEDIA_ROOT, 'pdf_images'),
+        upload_to=UPLOAD_TO_DIR,
         blank=True, null=True,
         editable=False,
     )
@@ -41,16 +45,51 @@ class PDFPluginModel(CMSPlugin):
     def save(self, *args, **kwargs):
         """Customized to generate an image from the pdf file."""
         # open image from pdf
-        img = Image(filename=self.file.path)
+        img = Image(filename=self.file.path + '[1]')
+
         # make new filename
         filename = os.path.basename(self.file.path).split('.')[:-1]
         if type(filename) == list:
             filename = ''.join(filename)
+
+        # TODO: Would be better to compute this path from the upload_to
+        # setting which is already set on the model field
+        image_dir = os.path.join(
+            settings.MEDIA_ROOT, UPLOAD_TO_DIR)
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+
         image_path = os.path.join(
-            settings.MEDIA_ROOT, 'pdf_images', '{}.jpg'.format(filename))
-        # save as image under new filename
-        img.save(filename=image_path)
+            image_dir, '{}.jpg'.format(filename))
+        tmp_image_path = os.path.join(
+            image_dir, '{}.tmp.jpg'.format(filename))
+
+        # we remove the old image befor we save because the cover might have
+        # changed when we upload a new PDF file - even when that file has the
+        # same filename as the old one
+        try:
+            os.remove(image_path)
+        except OSError:
+            # file is already gone
+            pass
+        # and we also remove the thumbnails
+        old_files = glob.glob('{}.*'.format(image_path))
+        for old_file in old_files:
+            try:
+                os.remove(old_file)
+            except OSError:
+                pass
+
+        # save as image under a temporary filename so that we can read it with
+        # File()
+        img.save(filename=tmp_image_path)
         # attach it to image field
-        with open(image_path, 'r') as f:
-            self.image.save(filename, File(f), save=False)
+        with open(tmp_image_path, 'r') as f:
+            self.image.save('{}.jpg'.format(filename), File(f), save=False)
         super(PDFPluginModel, self).save(*args, **kwargs)
+
+        # remove temp file
+        try:
+            os.remove(tmp_image_path)
+        except OSError:
+            pass
